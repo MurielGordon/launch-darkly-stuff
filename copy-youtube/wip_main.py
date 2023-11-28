8c6fdeb836110b6e98f8cb024e2d9fbce865ac57
Muriel Gordon <murielgordon6@gmail.com>
2023-11-28 15:41:37 -0800

    updated user to context and removed all but render home.html

---
+++ copy-youtube/wip_main.py
@@ -0,0 +1,35 @@
+from flask.templating import render_template
+from flask import Flask
+import ldclient
+from ldclient.config import Config 
+
+app = Flask(__name__)
+
+#is this still relevant? I think v9 has something about not needing users
+context = {
+    "kind": "user",
+    "key": "example-user-key",
+    "name": "Sandy"
+}
+
+
+@app.route("/")
+def get_feature():
+    # REMOVE BEFORE POSTING
+    ldclient.set_config(Config("*****"))
+    show_feature = ldclient.get().variation("launchTheme", context, False)
+    if show_feature == True:
+    #    show_ripley = ldclient.get().variation("launchRipley", user, "alien.jpg")
+    #    print(show_ripley)
+    #    ldclient.get().close()
+        return render_template("home.html")#, ripley=show_ripley)
+    #else:
+    #    ldclient.get().close()
+    #    return """
+    #    <h1>Welcome to Python and LaunchDarkly</h1>
+    #    <h2>This is highly boring and not on brand</h2>
+    #    <h2>:(</h2>
+    #    """
+
+if __name__ == '__main__':
+    app.run(debug=True)
